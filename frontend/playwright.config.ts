4fcd5b34c88f5327e3da24d80b9135abeed652ea
Hannah Liao <128202377+hannliao@users.noreply.github.com>
2025-04-05 19:39:30 -0600

    Add Playwright CI/CD pipeline

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -41,7 +41,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run test',
+    command: 'npm run start:servers',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
   },
